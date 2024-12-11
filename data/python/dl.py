7329dcb7b6f6a9d64cfdf5d7b8b50a19b24513d2
045200 <wsir9527@gmail.com>
2024-12-11 15:27:31 +0800

    更新 dl.py

--- data/python/dl.py
+++ data/python/dl.py
@@ -41,6 +41,8 @@ adblock = [
 
 "https://raw.hellogithub.com/hosts",
 
+"https://raw.gitmirror.com/lingeringsound/10007_auto/master/reward",
+
 "https://raw.githubusercontent.com/PhoenixLjw/AdRules/main/adguard-home.txt",
 
 "https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt",
